29c3e944ed86914fce4d53510e6f7acaf2a41808
hishamss <hishamss90@gmail.com>
2020-11-10 13:15:04 -0700

    first commit

---
+++ schema.sql
@@ -0,0 +1,35 @@
+-- first, create database called "Bank_P0"
+
+CREATE TABLE branches
+(
+    BranchID    serial primary key,
+    BranchName        VARCHAR(30) not null,
+    BranchCity       VARCHAR(30) not null
+);
+
+CREATE TABLE users
+(
+    UserID    serial primary key,
+	BranchID	integer,
+    FirstName        VARCHAR(30) not null,
+    LastName       VARCHAR(30) not null,
+	Email			VARCHAR(30) unique not null,
+	pass		VARCHAR(80) not null,
+	DOB 	date not null,
+	constraint fk_branchID
+     foreign key (BranchID) 
+     REFERENCES branches (BranchID)
+);
+
+CREATE TABLE accounts
+(
+    AccountID    serial primary key,
+	UserID 			integer,
+	AccountNumber VARCHAR(9) not null,
+    Balance        numeric(6, 2) not null,
+    OpenDate		date not null default current_date,
+	Status			VARCHAR(10) not null,
+	constraint fk_userid
+     foreign key (UserID) 
+     REFERENCES users (UserID)
+);
