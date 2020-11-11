8f1a51d8a9f2ff38aa43c0d9eef26b448bee5777
hishamss <hishamss90@gmail.com>
2020-11-10 18:11:53 -0700

    created account model class

---
+++ BankP0/src/bank/model/account.java
@@ -0,0 +1,50 @@
+package bank.model;
+
+public class account {
+ 
+	private int userID;
+	private String accountNumber;
+	private double balance;
+	private String status;
+	
+	public account(int userID, String accountNumber, double balance, String status) {
+		this.userID = userID;
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.status = status;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
