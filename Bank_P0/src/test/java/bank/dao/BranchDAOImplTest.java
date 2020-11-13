ca0adbf7a77bafff6c580a655a4bfbdd2e1c2a2c
hishamss <hishamss90@gmail.com>
2020-11-12 22:09:49 -0700

    converted the project to maven and added Junit test

---
+++ Bank_P0/src/test/java/bank/dao/BranchDAOImplTest.java
@@ -0,0 +1,14 @@
+package bank.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BranchDAOImplTest {
+
+	@Test
+	public void testAddBranch() {
+		fail("Not yet implemented");
+	}
+
+}
