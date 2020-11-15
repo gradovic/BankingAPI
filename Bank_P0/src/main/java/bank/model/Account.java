52541d7b75caaad563bf4d4ac448b6626f7e4948
hishamss <hishamss90@gmail.com>
2020-11-15 16:00:13 -0700

    deleted the account number field from account object

--- Bank_P0/src/main/java/bank/model/Account.java
+++ Bank_P0/src/main/java/bank/model/Account.java
@@ -3,16 +3,26 @@ package bank.model;
 public class Account {
  
 	private int userID;
-	private String accountNumber;
 	private double balance;
 	private String status;
 	
-	public Account(int userID, String accountNumber, double balance, String status) {
+	public Account(int userID, double balance, String status) {
 		this.userID = userID;
-		this.accountNumber = accountNumber;
 		this.balance = balance;
 		this.status = status;
 	}
+	
+	
+
+	public Account() {
+		super();
+		this.userID = -1;
+		this.balance = 0;
+		this.status = null;
+				
+	}
+
+
 
 	public int getUserID() {
 		return userID;
