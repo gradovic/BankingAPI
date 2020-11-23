849de0bb998a874d02f43415e27b7da44597407c
hishamss <hishamss90@gmail.com>
2020-11-23 13:54:05 -0700

    added docker compose yml file to run postgres DB on docker container as an option

--- Bank_P0/src/main/java/bank/utilities/DAOUtilities.java
+++ Bank_P0/src/main/java/bank/utilities/DAOUtilities.java
@@ -7,9 +7,16 @@ import java.sql.SQLException;
 //Class to implement database connection
 public class DAOUtilities {
 
-	private static final String URL = System.getenv("DB_URL"); // Env Variable for DB URL
-	private static final String CONNECTION_USERNAME = System.getenv("DB_USERNAME"); // Env Variable for DB username
-	private static final String CONNECTION_PASSWORD = System.getenv("DB_PASSWORD"); // Env Variable for DB password
+	
+	  private static final String URL = System.getenv("DB_URL"); // Env Variable for DB URL 
+	  private static final String CONNECTION_USERNAME = System.getenv("DB_USERNAME"); // Env Variable for DB username 
+	  private static final String CONNECTION_PASSWORD = System.getenv("DB_PASSWORD"); // Env Variable for DB password
+	 	
+	// PostgreSQL on docker
+	//private static final String URL_DOCKER = System.getenv("URL_DOCKER"); // Env Variable for DB URL
+	//private static final String CONNECTION_USERNAME_DOCKER = System.getenv("CONNECTION_USERNAME_DOCKER"); // Env Variable for DB username
+	//private static final String CONNECTION_PASSWORD_DOCKER = System.getenv("CONNECTION_PASSWORD_DOCKER"); // Env Variable for DB password
+	
 	private static Connection connection;
 
 	public static synchronized Connection getConnection() throws SQLException {
@@ -21,12 +28,16 @@ public class DAOUtilities {
 				e.printStackTrace();
 			}
 			connection = DriverManager.getConnection(URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
+			// PostgreSQL on Docker
+			//connection = DriverManager.getConnection(URL_DOCKER, CONNECTION_USERNAME_DOCKER, CONNECTION_PASSWORD_DOCKER);
 		}
 
 		// If connection was closed then retrieve a new connection
 		if (connection.isClosed()) {
 			System.out.println("Opening new connection...");
 			connection = DriverManager.getConnection(URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
+			// PostgreSQL on Docker
+			//connection = DriverManager.getConnection(URL_DOCKER, CONNECTION_USERNAME_DOCKER, CONNECTION_PASSWORD_DOCKER);
 		}
 		return connection;
 	}
